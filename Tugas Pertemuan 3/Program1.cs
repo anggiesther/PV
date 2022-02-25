f2b78646909011186351a185074d33fb688ae7be
anggiesther <85999694+anggiesther@users.noreply.github.com>
2022-02-26 01:12:19 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/Program1.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 2/25/2022
+ * Time: 10:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace belajar
+{
+	class ProgramHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama kamu siapa?");
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama kamu " + nama);
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
